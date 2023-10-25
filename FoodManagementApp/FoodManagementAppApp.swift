8b5d3a81430d77cdd04578db6d8dc97ae56b5c8c
f-sara <jyujyusa.68@gmail.com>
2023-10-25 11:09:56 +0900

    Initial Commit

---
+++ FoodManagementApp/FoodManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodManagementAppApp.swift
+//  FoodManagementApp
+//
+//  Created by 藤井紗良 on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
